150c76b0e9897bca95b52c433c6a3bc435d27029
nehal yadav <nehalyadav@nehals-MacBook-Air.local>
2026-07-03 00:07:19 +0530

    Add remaining Week 1 exercises

---
+++ Week 1/Data Structures and Algorithms/Exercise 7/Financial Forecasting/Exercise7Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        double initialValue = 10000.0;
+        double growthRate = 0.10; // 10%
+        int years = 5;
+
+        double futureValue = FinancialForecast.forecast(initialValue, growthRate, years);
+
+        System.out.println("Initial Value : " + initialValue);
+        System.out.println("Growth Rate   : " + (growthRate * 100) + "%");
+        System.out.println("Years         : " + years);
+        System.out.printf("Forecast Value: %.2f%n", futureValue);
+    }
+}
